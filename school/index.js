f0749a510f8b7ea2236b79b3e8cc95a07710e16f
yulongcs <yulongcs@qq.com>
2015-07-28 23:51:40 +0800

    create node App
    Learn nodejs

---
+++ school/index.js
@@ -0,0 +1,13 @@
+var klass = require('./klass');
+
+klass.add('Scott', ['stuA', 'stuB']);
+
+// exports.add = function(klasses) {
+// 	klasses.forEach(function(item, index) {
+// 		var _klass = item;
+// 		var teacherName = item.teacherName;
+// 		var studets = item.studets;
+
+// 		klass.add(teacherName, studets);
+// 	});
+// };
